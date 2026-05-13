2df2b288c819f91c3117b5cc199834d5225eed37
abdulmeliktuhaye <abdulmelikmohammednur123@gmail.com>
2026-05-13 16:07:50 -0500

    Initial Commit

--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -1,4 +1,15 @@
 package com.pluralsight;
 
 public class NameFormatter {
+     //Private constructor prevents anyone from creating a NameFormatter object
+    //This class is only supposed to contain static methods
+
+    private NameFormatter() {
+    }
+        //This method formats a first and last name
+        public static String format (String firstName, String lastName) {
+
+        //Return the name in the required format.
+            return lastName + " , " + firstName;
+    }
 }
